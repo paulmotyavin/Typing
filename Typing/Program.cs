186301d1675532fc21d3b9915ef2b58f53b2e9ea
paulmotyavin <isip_p.d.motyavin@mpt.ru>
2022-12-01 22:37:27 +0300

    Скоропечатание

---
+++ Typing/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Typing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Typing! I will do this mb");
+        }
+    }
+}
